6375e0148e2da38eff07e71aab478ab65b8f0bc4
Sahla Thasni <sahlathasni.p93@gmail.com>
2026-06-14 03:54:22 +0400

    tested product page

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,16 +1,30 @@
+// import ProductList from "@/components/product-list/ProductList";
+// import { ProductService } from "@/services/product-service";
+
+// export default function Product({ products }: any) {
+//   return <ProductList products={products} />;
+// }
+
+// export async function getServerSideProps() {
+//   const products = await ProductService.getproducts();
+
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
 import ProductList from "@/components/product-list/ProductList";
-import { ProductService } from "@/services/product-service";
 
-export default function Product({ products }: any) {
-  return <ProductList products={products} />;
-}
-
-export async function getServerSideProps() {
-  const products = await ProductService.getproducts();
-
-  return {
-    props: {
-      products,
+export default function Product() {
+  const products = [
+    {
+      id: 1,
+      title: "Test Product",
+      price: 100,
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
     },
-  };
+  ];
+
+  return <ProductList products={products} />;
 }
