13161d35bbc86b119f89187ecdd674c9d06df3b9
Sahla Thasni <sahlathasni.p93@gmail.com>
2026-06-14 03:32:56 +0400

    modified service file

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,14 +1,18 @@
 import { ServiceBase } from "./service-base";
 
 export class ProductService extends ServiceBase {
+  
   static getproducts = async () => {
-    var productsResponse = await fetch(this.getUrl(`/products`), {
-      cache: "no-store",
-    });
-    var products = await productsResponse.json();
+  const url = this.getUrl("/products");
+  console.log("URL =", url);
 
-    return products;
-  };
+  const response = await fetch(url);
+
+  const text = await response.text();
+  console.log(text.substring(0, 100));
+
+  return JSON.parse(text);
+};
   static getProductById = async (id: number) => {
     var productResponse = await fetch(this.getUrl(`/products/` + id));
     var product = await productResponse.json();
