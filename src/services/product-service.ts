0424fcff773c5ef077bcc51ce7ff0e5e8b4356c0
Sahla Thasni <sahlathasni.p93@gmail.com>
2026-06-14 13:54:12 +0400

    testing product sevice page

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,15 +1,26 @@
+// import { ServiceBase } from "./service-base";
+
+// export class ProductService extends ServiceBase {
+  
+// static getproducts = async () => {
+//   const response = await fetch(this.getUrl("/products"));
+
+//   return await response.json();
+// };
+// static getProductById = async (id: number) => {
+//   const response = await fetch(this.getUrl(`/products/${id}`));
+
+//   return await response.json();
+// };
+// }
 import { ServiceBase } from "./service-base";
 
 export class ProductService extends ServiceBase {
-  
-static getproducts = async () => {
-  const response = await fetch(this.getUrl("/products"));
+  static async getproducts() {
+    return this.get("/products");
+  }
 
-  return await response.json();
-};
-static getProductById = async (id: number) => {
-  const response = await fetch(this.getUrl(`/products/${id}`));
-
-  return await response.json();
-};
+  static async getProductById(id: number) {
+    return this.get(`/products/${id}`);
+  }
 }
